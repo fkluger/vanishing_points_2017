88a42553e541788dd89697f226135c15df844244
Florian Kluger <kluger@tnt.uni-hannover.de>
2019-04-18 15:53:20 +0200

    minor fix

--- lsdpython/setup.py
+++ lsdpython/setup.py
@@ -2,8 +2,9 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 setup(
     cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension("lsd", ["lsd.pyx", 'lsd_1.6/lsd.c'])]
+    ext_modules=[Extension("lsd", ["lsd.pyx", 'lsd_1.6/lsd.c'], include_dirs=[numpy.get_include()])]
 )
